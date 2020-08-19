8b99d24e97a54f86f1f731ef2ce86c162fd098f3
kguamanzara <52337852+kguamanzara@users.noreply.github.com>
2020-08-19 00:05:35 -0500

    Add files via upload

---
+++ Paralelo1/hilos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+ 
+namespace Programacion.Hilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+			
+        {
+            var EContratista = new Contratista();  
+            Thread hilo1 = new Thread(EContratista.ImprimeEnunciado);
+            Thread hilo2 = new Thread(EContratista.ImprimeEnunciado);
+             Console.WriteLine("ID Hilo: {0} => Se llama a la ejecución de los hilos desde el programa principal", Thread.CurrentThread.ManagedThreadId.ToString());
+ 
+           
+            hilo1.Start("Empresa: El 'Contratista tiene que firmar el documento");
+            hilo2.Start("Persona:Enseguida preparo los documentos");
+            
+            while ((hilo1.IsAlive) || (hilo2.IsAlive)) // Se espera a que termine la ejecución de ambos hilos
+            {
+            }
+ 
+            Console.WriteLine("ID Hilo: {0} => Contrato finalizado", Thread.CurrentThread.ManagedThreadId.ToString());
+            Console.ReadLine();
+        }
+    }
+ 
+    // Clase de apoyo que contiene la función que ejecutarán los hilos
+    public class Contratista
+    {
+        public void ImprimeEnunciado(object texto)
+        {
+            
+            Console.WriteLine("ID Hilo: {0} => {1}.", Thread.CurrentThread.ManagedThreadId.ToString(), texto.ToString());
+        }
+    }
+}
---
+++ Paralelo1/paralelismo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Paralelo
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] Numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20 };  //Crear un array
+            Console.Write("Normal foreach: ");
+            foreach (var numeros in Numeros)
+            {
+                Console.Write(longCalculation(numeros));   //Impresion del 1 al 1000
+            }
+           
+            Console.Write("Paralelismo foreach : ");    //Salida en desorden
+            Parallel.ForEach(Numeros, numeros => {  
+                Console.WriteLine(longCalculation(numeros));
+            });
+        }
+
+        private static int longCalculation(int numeros)
+        {
+            Thread.Sleep(1000); 
+            return numeros * numeros;
+        }
+    }
+}
